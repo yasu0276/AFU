553958f745c4234396deb5fcde1b7cb5028ac754
Yasu Yoshida <w.yasu.yoshida@gmail.com>
2024-11-19 09:48:02 +0900

    1. ビルド用スクリプトの修正

--- build.py
+++ build.py
@@ -10,24 +10,27 @@ if __name__=="__main__":
     if os.path.exists("./dist"): shutil.rmtree("./dist")
 
     print("** build app **")
+    app_name = "afu"
     if os.name == "nt":
+        app_name = "{}-windows.exe".format(app_name)
         output_path = "./dist/win"
         # ディレクトリが存在しない場合は作成
         if not os.path.exists(output_path): os.makedirs(output_path)
-        result = subprocess.run("pyinstaller --windowed --distpath={} --onefile --noconsole --name main ./src/main.py --collect-data TkEasyGUI -y".format(output_path), shell=True)
+        result = subprocess.run("pyinstaller --windowed --distpath={} --onefile --noconsole --name afu-windows.exe ./src/main.py --collect-data TkEasyGUI -y".format(output_path), shell=True)
     else:
+        app_name = "{}-mac".format(app_name)
         output_path = "./dist/mac"
         # ディレクトリが存在しない場合は作成
         if not os.path.exists(output_path): os.makedirs(output_path)
-        result = subprocess.run(["pyinstaller --windowed --distpath={} --onefile --noconsole --name main ./src/main.py --collect-data TkEasyGUI -y".format(output_path)], shell=True)
+        result = subprocess.run(["pyinstaller --windowed --distpath={} --onefile --noconsole --name afu-macos ./src/main.py --collect-data TkEasyGUI -y".format(output_path)], shell=True)
 
     if (result.returncode != 0):
         print("** build error **")
         sys.exit()
 
     print("** clean up the directory **")
-    is_file = os.path.isfile("./*.spec")
-    if is_file: os.remove("./*.spec")
+    is_file = os.path.isfile("{}.spec".format(app_name))
+    if is_file: os.remove("{}.spec".format(app_name))
 
     is_dir = os.path.isdir("./build")
     if is_dir: shutil.rmtree('./build')
