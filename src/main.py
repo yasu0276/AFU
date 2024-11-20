99d088bcdc37af5ae5613a1b1523c620c9cd5eea
Yasu Yoshida <w.yasu.yoshida@gmail.com>
2024-11-20 09:26:52 +0900

    TkEasyGUI から tkinterdnd2 へ乗り換え

--- src/main.py
+++ src/main.py
@@ -1,25 +1,52 @@
-import TkEasyGUI as eg
+import tkinter as tk
+from tkinterdnd2 import DND_FILES, TkinterDnD
 
-# Create window
-layout = [
-    [eg.Text("file:")],
-    [
-        eg.Input(key="-file-"),
-        eg.FileBrowse(),  # FileBrowse button
-        eg.Button("Copy", key="-copy-file-"),
-    ],
-    [eg.CloseButton()],
-]
+class MyApp(TkinterDnD.Tk):
+    def __init__(self):
+        super().__init__()
 
-if __name__=="__main__":
-    window = eg.Window("FileBrowse Test", layout, size=(500, 300))
+        ## ウィンドウサイズ
+        width = 1000
+        height = 600
+        self.geometry(f'{width}x{height}')
+        self.minsize(width, height)
+        self.maxsize(width, height)
+        self.title(f'AFU')
 
-    # Event loop
-    while window.is_running():
-        # get window event
-        event, values = window.read()
-        print("@@@", event, values)
-        if event == "-copy-file-":
-            eg.set_clipboard(values["-file-"])
-            eg.print("Copied to clipboard:\n", values["-file-"])
-    window.close()
+        ## フレーム
+        self.frame_drag_drop = frameDragAndDrop(self)
+
+        ## 配置
+        self.frame_drag_drop.grid(column=0, row=0, padx=5, pady=5, sticky=(tk.E, tk.W, tk.S, tk.N))
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+class frameDragAndDrop(tk.LabelFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.textbox = tk.Text(self)
+        self.textbox.insert(0.0, "Drag and Drop File.")
+        self.textbox.configure(state='disabled')
+
+        ## ドラッグアンドドロップ
+        self.textbox.drop_target_register(DND_FILES)
+        self.textbox.dnd_bind('<<Drop>>', self.funcDragAndDrop)
+
+        ## スクロールバー設定
+        self.scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL, command=self.textbox.yview)
+        self.textbox['yscrollcommand'] = self.scrollbar.set
+
+        ## 配置
+        self.textbox.grid(column=0, row=0, sticky=(tk.E, tk.W, tk.S, tk.N))
+        self.scrollbar.grid(row=0, column=1, sticky=(tk.N, tk.S))
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+    def funcDragAndDrop(self, e):
+        ## ここを編集してください
+        print(e.data)
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.mainloop()
+
